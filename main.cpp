452b9a9f4fa9f16f26e76d654dae321f24b3a7c1
Shaayyy <76052610+Shaayyy@users.noreply.github.com>
2021-01-02 07:10:13 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 int main()
 {
+    // Asking the user for input //
     string y_name;
     cout << "Enter your name: " ;
     cin>> y_name;
@@ -14,9 +15,11 @@ int main()
     string y_surname;
     cout << "Enter your surname: ";
     cin >> y_surname;
-
+    
+    // storing all info regarding 'you' in an array (for accessing via a complex random utility (; ) //
     string your [3] { y_name, yn_name, y_surname};
 
+    // asking user for input PART2 //
     string p_name;
     cout<< "Enter your partners name: ";
     cin>> p_name;
@@ -26,16 +29,20 @@ int main()
     string p_surname;
     cout << "Enter your partners surname: ";
     cin >> p_surname;
-
+  
+    // storing info regarding your partner in an array (for accessing via a complex random utility PART2 (; //
     string partner [3] {p_name, pn_name, p_surname};
 
-
+    // turned out useless ); // 
     string year;
     cout << "Enter your wedding year: ";
     cin >> year;
-
+    
+    // an array storing all of the 'hashtag words' // 
     string key [7] {"Weds" , "SayIDo", "TieTheKnot", "KissAndTell", "Loves", "GetHitched", "Forever", } ;
 
+    
+    // this is the complex part. we are doing mod by the array length so that we get indexes as results for the random function to access from the respective arrays // 
     for (int i=0; i<50; i++)
     {
         cout <<"#" << your[rand()%3] << key[rand()%7] << partner[rand()%3] << endl;
